494805e0583bd65673b3ae05e24701e4a8a76cdc
Brian Choi <bgchoi93@gmail.com>
2018-03-10 01:16:17 -0500

    setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='project-lff',
+    version='1.0.0',
+    packages=['questions', 'notes'],
+    url='',
+    license='',
+    author='Brian Choi',
+    author_email='bgchoi93@gmail.com',
+    description='Project : Learn From Failure'
+)
